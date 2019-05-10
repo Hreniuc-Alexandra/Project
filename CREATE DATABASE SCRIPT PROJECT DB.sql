7f12b0b2743e31f7899a88a6a3a38b3ca8c720a2
andreiLupascu <48789640+andreiLupascu@users.noreply.github.com>
2019-05-10 11:07:48 +0300

    Add files via upload
    SQL create database + tables script

---
+++ CREATE DATABASE SCRIPT PROJECT DB.sql
@@ -0,0 +1,54 @@
+create table customer 
+	(
+		id 					bigint not null auto_increment, 
+		email 				varchar(255), 
+		first_name 			varchar(255), 
+		last_name 			varchar(255), 
+		token 				varchar(255), 
+		primary key 		(id)
+	) engine=InnoDB
+create table dish 
+	(
+		id 					bigint not null auto_increment, 
+		cost 				double precision not null, 
+		name 				varchar(255), 
+		menu_id 			bigint, 
+		primary key 		(id)
+	) engine=InnoDB
+create table menu 
+	(
+		id 					bigint not null auto_increment, 
+		discount_percent	double precision, 
+		primary key 		(id), 
+		check 				(discount_percent<15)
+	) engine=InnoDB
+create table order_item 
+	(
+		id 					bigint not null auto_increment, 
+		quantity 			integer, 
+		dish_id 			bigint, 
+		purchase_id 		bigint, 
+		primary key 		(id)
+	) engine=InnoDB
+create table purchase 
+	(
+	id 						bigint not null auto_increment, 
+	date 					datetime, 
+	customer_id 			bigint, 
+	primary key 			(id)
+	) engine=InnoDB
+create table restaurant 
+	(
+	id 						bigint not null auto_increment, 
+	image_url 				varchar(255), 
+	name 					varchar(255), 
+	menu_id 				bigint not null, 
+	primary key 			(id)
+	) engine=InnoDB
+	
+alter table customer add constraint UK_dwk6cx0afu8bs9o4t536v1j5v unique (email)
+alter table dish add constraint FKljuksxg35var0r9a3y09l148h foreign key (menu_id) references menu (id)
+alter table order_item add constraint FKs7aplknkrukmckr29wijlvcy1 foreign key (dish_id) references dish (id)
+alter table order_item add constraint FKop179ve526cnm1etgba1xjhl5 foreign key (purchase_id) references purchase (id)
+alter table purchase add constraint FK2pehe23hwdcyql94c531rbf70 foreign key (customer_id) references customer (id)
+alter table restaurant add constraint FKooiid4751i6wv4ebwbi6gjfr3 foreign key (menu_id) references menu (id)
