cbafb2371938f74f0fa172124bebad92d03f959e
andreiLupascu <48789640+andreiLupascu@users.noreply.github.com>
2019-05-26 17:21:48 +0300

    updated script to be usable as a whole

--- CREATE DATABASE SCRIPT PROJECT DB.sql
+++ CREATE DATABASE SCRIPT PROJECT DB.sql
@@ -6,7 +6,7 @@
 			last_name 		varchar(255), 
 			token 			varchar(255), 
 			primary key 		(id)
-		) engine=InnoDB
+		) engine=InnoDB;
 	create table dish 
 		(
 			id 			bigint not null auto_increment, 
@@ -14,14 +14,14 @@
 			name 			varchar(255), 
 			menu_id 		bigint, 
 			primary key 		(id)
-		) engine=InnoDB
+		) engine=InnoDB;
 	create table menu 
 		(
 			id 			bigint not null auto_increment, 
 			discount_percent	double precision, 
 			primary key 		(id), 
 			check 			(discount_percent<15)
-		) engine=InnoDB
+		) engine=InnoDB;
 	create table order_item 
 		(
 			id 			bigint not null auto_increment, 
@@ -29,7 +29,7 @@
 			dish_id 		bigint, 
 			purchase_id 		bigint, 
 			primary key 		(id)
-		) engine=InnoDB
+		) engine=InnoDB;
 	create table purchase 
 		(
 		id 				bigint not null auto_increment, 
@@ -37,7 +37,7 @@
 		extra_fees 			double precision,
 		customer_id 			bigint, 
 		primary key 			(id)
-		) engine=InnoDB
+		) engine=InnoDB;
 	create table restaurant 
 		(
 		id 				bigint not null auto_increment, 
@@ -45,12 +45,12 @@
 		name 				varchar(255), 
 		menu_id 			bigint not null, 
 		primary key 			(id)
-		) engine=InnoDB
+		) engine=InnoDB;
 		
-	alter table customer add constraint UK_dwk6cx0afu8bs9o4t536v1j5v unique (email)
-	alter table customer add constraint UK_i1p08swb0onyuxes5tjx73rqg unique (token)
-	alter table dish add constraint FKljuksxg35var0r9a3y09l148h foreign key (menu_id) references menu (id)
-	alter table order_item add constraint FKs7aplknkrukmckr29wijlvcy1 foreign key (dish_id) references dish (id)
-	alter table order_item add constraint FKop179ve526cnm1etgba1xjhl5 foreign key (purchase_id) references purchase (id)
-	alter table purchase add constraint FK2pehe23hwdcyql94c531rbf70 foreign key (customer_id) references customer (id)
-	alter table restaurant add constraint FKooiid4751i6wv4ebwbi6gjfr3 foreign key (menu_id) references menu (id)
+	alter table customer add constraint UK_dwk6cx0afu8bs9o4t536v1j5v unique (email);
+	alter table customer add constraint UK_i1p08swb0onyuxes5tjx73rqg unique (token);
+	alter table dish add constraint FKljuksxg35var0r9a3y09l148h foreign key (menu_id) references menu (id);
+	alter table order_item add constraint FKs7aplknkrukmckr29wijlvcy1 foreign key (dish_id) references dish (id);
+	alter table order_item add constraint FKop179ve526cnm1etgba1xjhl5 foreign key (purchase_id) references purchase (id);
+	alter table purchase add constraint FK2pehe23hwdcyql94c531rbf70 foreign key (customer_id) references customer (id);
+	alter table restaurant add constraint FKooiid4751i6wv4ebwbi6gjfr3 foreign key (menu_id) references menu (id);
